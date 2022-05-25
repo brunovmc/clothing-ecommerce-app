e3c008302cbb06e437ab71a661e8303d7b2ede36
brunovmc <bruno.vidigal.coriolano@gmail.com>
2022-05-25 13:47:17 -0300

    created Directory component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,37 @@
-import CategoryItem from './components/category-item/category-item.component';
-import './categories.styles.scss';
+import Directory from './components/directory/directory.component';
  
 const App = () => {
 
   const categories = [
   {
     "id": 1,
-    "title": "hats",
+    "title": "Hats",
     "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
   },
   {
     "id": 2,
-    "title": "jackets",
+    "title": "Jackets",
     "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
   },
   {
     "id": 3,
-    "title": "sneakers",
+    "title": "Sneakers",
     "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
   },
   {
     "id": 4,
-    "title": "womens",
+    "title": "Womens",
     "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
   },
   {
     "id": 5,
-    "title": "mens",
+    "title": "Mens",
     "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
   }
   ];
 
   return(
-    <div className="categories-container">
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category}/>
-      ))}
-    </div>
+    <Directory categories={categories} />
   );
 };
 
