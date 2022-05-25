4971b5476f6adeea3131c3e9e8c9da3f79102060
brunovmc <bruno.vidigal.coriolano@gmail.com>
2022-05-25 19:31:21 -0300

    firebase user utils working

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -37,4 +37,24 @@ export const createUserDocumentFromAuth = async (userAuth) => {
     const userDocRef = doc(db, 'users', userAuth.uid);
     const userSnapshot = await getDoc(userDocRef);
     
+    if(!userSnapshot.exists()){
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        } catch (error) {
+            console.log('error creating the user', error.message)
+        }
+        return userDocRef;
+    }
+
+    //if user does not exist
+
+
+
+
 }
