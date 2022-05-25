d204e234da99af26bfad22fb8e2b042a2e5b5c7f
brunovmc <bruno.vidigal.coriolano@gmail.com>
2022-05-25 17:11:24 -0300

    created signin page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route} from  'react-router-dom'
 
 import NavBar from './routes/navbar/navbar.component';
 import Home from  './routes/home/home.component'
+import SignIn from './routes/signin/signin.component';
 
 
 const Shop = () => {
@@ -16,6 +17,7 @@ const App = () => {
       <Route path='/' element={<NavBar />} >
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
       </Route>
     </Routes>
   );
