2a6608f322f1f54600b5abbc8c8590ba0d28a087
brunovmc <bruno.vidigal.coriolano@gmail.com>
2022-05-25 18:16:19 -0300

    firebase auth implemented

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,27 @@
+import { initializeApp } from 'firebase/app';
+import { 
+    getAuth, 
+    signInWithRedirect, 
+    signInWithPopup, 
+    GoogleAuthProvider
+} from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCSV1MAovSJBqN2iVnG8GNCqbL5YcYssfU",
+  authDomain: "clothing-ecommerce-app-dummydb.firebaseapp.com",
+  projectId: "clothing-ecommerce-app-dummydb",
+  storageBucket: "clothing-ecommerce-app-dummydb.appspot.com",
+  messagingSenderId: "1002804874926",
+  appId: "1:1002804874926:web:71b67b67b092cb049c673f"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+    prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
