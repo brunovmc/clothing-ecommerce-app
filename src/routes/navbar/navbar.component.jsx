6828c99081cfef99ff56d451901d47c77bcb7465
brunovmc <bruno.vidigal.coriolano@gmail.com>
2022-05-25 16:53:46 -0300

    navbar styles and logo

--- src/routes/navbar/navbar.component.jsx
+++ src/routes/navbar/navbar.component.jsx
@@ -1,12 +1,13 @@
 import { Fragment } from 'react';
 import { Outlet, Link } from  'react-router-dom';
-
+import { ReactComponent as BrandLogo} from '../../assets/crown.svg';
+import './navbar.styles.scss'
 const NavBar = () => {
   return (
     <Fragment>
       <div className='navbar'>
         <Link className='logo-container' to='/'>
-            <h1>Brand</h1>
+            <BrandLogo className='logo' />
         </Link>
         <div className='nav-links-container'>
             <Link className='nav-link' to='/shop'>
