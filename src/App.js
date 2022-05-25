f5febf1d5b372b72b97d0806817bed7a5ce83a09
brunovmc <bruno.vidigal.coriolano@gmail.com>
2022-05-25 16:20:06 -0300

    react router and routes begin

--- src/App.js
+++ src/App.js
@@ -1,37 +1,32 @@
-import Directory from './components/directory/directory.component';
- 
+import { Routes, Route, Outlet } from  'react-router-dom'
+
+import Home from  './routes/home/home.component'
+
+const NavBar = () => {
+  return (
+    <div>
+      <div>
+        <h1>Brand</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+}
+
+const Shop = () => {
+  return (
+    <h1>Shop Page!</h1>
+  );
+}
+
 const App = () => {
-
-  const categories = [
-  {
-    "id": 1,
-    "title": "Hats",
-    "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-  },
-  {
-    "id": 2,
-    "title": "Jackets",
-    "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-  },
-  {
-    "id": 3,
-    "title": "Sneakers",
-    "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-  },
-  {
-    "id": 4,
-    "title": "Womens",
-    "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-  },
-  {
-    "id": 5,
-    "title": "Mens",
-    "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-  }
-  ];
-
-  return(
-    <Directory categories={categories} />
+  return (
+    <Routes>
+      <Route path='/' element={<NavBar />} >
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
+    </Routes>
   );
 };
 
